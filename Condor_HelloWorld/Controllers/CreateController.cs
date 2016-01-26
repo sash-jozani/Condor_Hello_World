e29726f69c79fafc7cb72bc00e329d3178b2446c
Condo12 <hamed.beyraghdar@gmail.com>
2016-01-26 19:08:06 +0330

    a really quick login

--- Condor_HelloWorld/Controllers/CreateController.cs
+++ Condor_HelloWorld/Controllers/CreateController.cs
@@ -26,30 +26,30 @@ namespace Condor_HelloWorld.Controllers
         [HttpPost]
         public ActionResult Create(CreateViewModel createParameter)
         {
-            int counter = 0;
-            var cnt = Request.Cookies["counter"] == null ? string.Empty : Request.Cookies["counter"].Value;
+        //    int counter = 0;
+        //    var cnt = Request.Cookies["counter"] == null ? string.Empty : Request.Cookies["counter"].Value;
 
-            if (!string.IsNullOrEmpty(cnt))
-            {
-                counter =  Convert.ToInt32(cnt);
-                HttpCookie cookie = new HttpCookie("counter");
-                cookie.Value = (counter + 1).ToString();
-                cookie.Expires = DateTime.Now.AddDays(3);
-                Response.Cookies.Add(cookie);
-            }
-            else
-            {
-                HttpCookie cookie = new HttpCookie("counter");
-                cookie.Value = "0";
-                cookie.Expires = DateTime.Now.AddDays(3);
-                Response.Cookies.Add(cookie);
-            }
+        //    if (!string.IsNullOrEmpty(cnt))
+        //    {
+        //        counter =  Convert.ToInt32(cnt);
+        //        HttpCookie cookie = new HttpCookie("counter");
+        //        cookie.Value = (counter + 1).ToString();
+        //        cookie.Expires = DateTime.Now.AddDays(3);
+        //        Response.Cookies.Add(cookie);
+        //    }
+        //    else
+        //    {
+        //        HttpCookie cookie = new HttpCookie("counter");
+        //        cookie.Value = "0";
+        //        cookie.Expires = DateTime.Now.AddDays(3);
+        //        Response.Cookies.Add(cookie);
+        //    }
 
-            HttpCookie cook = new HttpCookie("cookiesForCreate_" + counter.ToString());
+            HttpCookie cook = new HttpCookie("cookiesForCreate");
             cook.Value = "email:" + createParameter.email + "#name:" + createParameter.fullName + "#password:" + createParameter.password;
             cook.Expires = DateTime.Now.AddDays(3);
             Response.Cookies.Add(cook);
-            return View();
+            return RedirectToAction("index");
         }
 
         public ActionResult logIn()
@@ -57,12 +57,12 @@ namespace Condor_HelloWorld.Controllers
             CreateViewModel model = new CreateViewModel();
             if (Request.Cookies["cookiesForCreate"] != null)
             {
-                string cookieValue = Request.Cookies["cookiesForCreate"].Value;
-                string[] readingThatBitch = cookieValue.Split('#');
-                model.email = readingThatBitch[0];
-                model.fullName = readingThatBitch[1];
-                model.password = readingThatBitch[2];
-                model.confirmationPassword = readingThatBitch[2];
+                //string cookieValue = Request.Cookies["cookiesForCreate"].Value;
+                //string[] readingThatBitch = cookieValue.Split('#');
+                //model.email = readingThatBitch[0].Replace("email:","");
+                //model.fullName = readingThatBitch[1];
+                //model.password = readingThatBitch[2];
+                //model.confirmationPassword = readingThatBitch[2];
             }
             else
             {
@@ -72,21 +72,32 @@ namespace Condor_HelloWorld.Controllers
             return View(model);
         }
         [HttpPost]
-        public ActionResult logIn() 
+        public ActionResult logIn(CreateViewModel loginParameter) 
         {
-            // Check user pass
-            // return View();
 
-            return RedirectToAction("Profile");
+            string enteredEmail = loginParameter.email;
+            string enteredPass = loginParameter.password;
+            string cookieValue = Request.Cookies["cookiesForCreate"].Value;
+            string[] readingThatBitch = cookieValue.Split('#');
+            string orginalEmail = readingThatBitch[0].Replace("email:", "");
+            string orginalPassword = readingThatBitch[2].Replace("password:", "");
 
-
+            if (enteredEmail == orginalEmail && enteredPass==orginalPassword)
+            {
+                return RedirectToAction("sucess");
+            }
+            else
+            {
+                CreateViewModel model = new CreateViewModel();
+                ViewBag.wrong = "true";
+                return View(model);
+            }
         }
 
-        public ActionResult Profile() 
+        public ActionResult sucess()
         {
             return View();
         }
-
 
     }
 }
