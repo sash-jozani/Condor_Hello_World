66ae0199718a0a7a3205f4082043c6a8fdfd1a04
Condo12 <hamed.beyraghdar@gmail.com>
2016-02-01 09:44:58 +0330

    Query string ham done

---
+++ Condor_HelloWorld/Controllers/QSController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Condor_HelloWorld.Models;
+
+
+namespace Condor_HelloWorld.Controllers
+{
+    public class QSController : Controller
+    {
+        //
+        // GET: /QS/
+
+        public ActionResult Index()
+        {
+            QSViewModel model = new QSViewModel();
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Index(QSViewModel model)
+        {
+
+            return RedirectToAction("searching", new { search = model.shitYouSearched });
+        }
+        public ActionResult searching()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Condor_HelloWorld/Models/QSViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Condor_HelloWorld.Models
+{
+    public class QSViewModel
+    {
+        [Required]
+        public string shitYouSearched { get; set; }
+    }
+}
